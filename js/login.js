a09682852ccc3c67621397c56286c3f3273f77ce
MrMengJ <2646973632@qq.com>
2017-03-28 17:31:36 +0800

    login.js添加一行注释

--- js/login.js
+++ js/login.js
@@ -28,6 +28,7 @@ $('.fastLogin p').on('click',function () {
 
 // 登录按钮显示效果
 //input propertychange是时时检测输入框中内容的变化，而change()是在输入框失去焦点以后才检测内容的变化
+// 这里其实还可以实用键盘按下事件keyup()来实现，但个人认为是实用了on('input propertychange',function (){})这种方案
 
 $('.import input').on('input propertychange',function () {
     var input1 = $('.securityCode input').val() !== '';
