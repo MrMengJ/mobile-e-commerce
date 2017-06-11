60a3e9ee202b8ac0307be453caff7d2c8f9ab154
MrMengJ <2646973632@qq.com>
2017-06-11 15:36:21 +0800

    注册页验证码倒计时
    给注册号码发送验证码后，给按钮显示倒计时60s，倒计时以后才能继续再次发送验证码

--- js/register.js
+++ js/register.js
@@ -1,25 +1,46 @@
 $(document).ready(function () {
 
-})
+});
 
 // 切换默认的隐藏的导航栏显示状态
 $('.icon_menu').on('click', function () {
     $('.header_bar').toggle();
-})
+});
 
 
 //获取验证码按钮变红
 // input propertychange是时时检测输入框中内容的变化，而change()是在输入框失去焦点以后才检测内容的变化
 $('.phoneNum input').on('input propertychange', function () {
-    if ($(this).val() !== '') {
-        $('.phoneNum .codeBtn').addClass('active')
-    }else{
+    if ($(this).val() !== '' && $('.phoneNum .codeBtn').text() == '获取手机验证码') {
+        $('.phoneNum .codeBtn').addClass('active');
+    } else {
         $('.phoneNum .codeBtn').removeClass('active')
     }
-})
+});
+
+//重新发送验证码
+$('.phoneNum .codeBtn').on('click', function () {
+    var _this = $(this);
+    var number = 60;
+    if (_this.hasClass('active')) {
+        _this.removeClass('active');
+        var countdown = setInterval(function () {
+          _this.text("重新发送（" + number + "s）");
+          number--;
+          if (number<0) {
+              _this.text("获取手机验证码");
+              if ($('.phoneNum input').val() !== '') {
+                  _this.addClass('active');
+              }
+              clearInterval(countdown);
+          }
+        }, 1000);
+    }
+});
+
 
 //所有选项都填写完毕，注册按钮变红
-$('.import input').on('input propertychange',function () {
+$('.import input').on('input propertychange', function () {
     var input1 = $('.securityCode input').val() !== '';
     var input2 = $('.phoneNum input').val() !== '';
     var input3 = $('.phoneNumCode input').val() !== '';
@@ -27,42 +48,42 @@ $('.import input').on('input propertychange',function () {
     if ((input1 && input2) && (input3 && input4)) {
         $('.registerBtn').addClass('active');
     }
-    else{
+    else {
         $('.registerBtn').removeClass('active');
     }
-})
+});
 
 // 输入正确的手机号码
 $('.phoneNum input').blur(function () {
     var truePhoneNum = /^1[3|4|5|7|8]\d{9}$/;
-    if(!(truePhoneNum.test($(this).val()))){
-        $('.phoneNum p').text('请输入正确的手机号码')
+    if (!(truePhoneNum.test($(this).val()))) {
+        $('.phoneNum p').text('请输入正确的手机号码');
         $(this).val('');
         $('.phoneNum .codeBtn').removeClass('active')
         $('.registerBtn').removeClass('active');
-    }else{
-        $('.phoneNum p').text('')
+    } else {
+        $('.phoneNum p').text('');
     }
-})
+});
 
 
 // 限制密码长度为6到20位长度
 $('.password input').blur(function () {
     var truePassword = /^\S{6,20}$/;
-    if (!(truePassword.test($(this).val()))){
+    if (!(truePassword.test($(this).val()))) {
         $('.password p').text('登录密码长度应为6到20位');
         $(this).val('');
         $('.registerBtn').removeClass('active');
-    }else{
+    } else {
         $('.password p').text('');
     }
-})
+});
 
 // 是否可以注册了
-$(".registerBtn").on('click',function () {
-    if ($(this).hasClass('active')){
+$(".registerBtn").on('click', function () {
+    if ($(this).hasClass('active')) {
         var url = $('.registerBtn').data('href');
-        $('.registerBtn').attr('href',url)
+        $('.registerBtn').attr('href', url)
     }
-})
+});
 
